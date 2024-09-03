143e9c3912aa72f6e8a4cacff58b9682b5a4d89f
Johann Schopplich <mail@johannschopplich.com>
2024-09-03 09:34:50 +0200

    chore: add some comments for random ID generation

--- src/string.ts
+++ src/string.ts
@@ -23,14 +23,16 @@ export function template(
 /**
  * Generates a random string.
  *
- * @remarks Ported from nanoid.
+ * @remarks Ported from `nanoid`.
  * @see https://github.com/ai/nanoid
  */
 export function generateRandomId(size = 16, dict = URL_ALPHABET) {
   let id = ''
+  // A compact alternative for `for (var i = 0; i < step; i++)`.
   let i = size
   const len = dict.length
   while (i--)
+    // `| 0` is more compact and faster than `Math.floor()`.
     id += dict[(Math.random() * len) | 0]
   return id
 }
